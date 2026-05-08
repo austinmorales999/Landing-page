f4fdb09ac189534e73c9963079a2498bc9e7a6a6
Austin Morales <harrenaustintm@gmail.com>
2026-05-08 12:46:00 -0400

    removed typescript dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Landing from './pages/Landing'
+import Landing from './pages/Landing';
 import { BrowserRouter as Router }  from 'react-router-dom' 
 export default function App() {
   return (
